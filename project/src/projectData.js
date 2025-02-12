4cdde4c6992836fe1a33568a61e46e2737343191
limdongsun0814 <118763659+limdongsun0814@users.noreply.github.com>
2025-02-12 17:50:51 +0900

    Update projectData.js

--- project/src/projectData.js
+++ project/src/projectData.js
@@ -24,7 +24,7 @@ const projects = [
       {
         titleDescription: "담당역할",
         descriptionList: [
-          "  DB 설계, 카드 해택, 크롤링, 딥러닝 모델 설계, CSS 및 기능 통합",
+          "  DB 설계, DB docker 배포, 회원가입 및 로그인 , 소셜로그인, 장바구니, 결제, 검색",
         ],
       },
     ],
